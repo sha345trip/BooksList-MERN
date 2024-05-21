462bc4d63a7943ccca08ee6333d289de3212c574
sha345trip <114864315+sha345trip@users.noreply.github.com>
2024-05-21 13:40:12 +0530

    first

---
+++ backend/config.js
@@ -0,0 +1,3 @@
+export const PORT=5555;
+export const mongoDBURL =
+    'mongodb+srv://root:root@bookstore.hpbvhrt.mongodb.net/book-Collection?retryWrites=true&w=majority&appName=BookStore'
---
+++ backend/index.js
@@ -0,0 +1,27 @@
+import express from "express";
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from "mongoose";
+import booksRoute from './routes/booksRoute.js'
+import cors from 'cors'
+
+const app= express();
+app.use(express.json());
+app.use(cors());
+
+app.get('/',(request, response)=>{
+    console.log(request)
+    return response.status(234).send('Hello world')
+});
+app.use('/books', booksRoute);
+
+mongoose
+    .connect(mongoDBURL)
+    .then(()=>{
+        console.log("App connected to database");
+        app.listen(PORT, () =>{
+            console.log(`App is listening at port: ${PORT}`)
+        });
+    })
+    .catch((error)=>{
+        console.log(error);
+    })
